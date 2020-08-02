7b27d21020c300ea0082830e8f9bf1172fa34a24
Hiroki Osame <hiroki.osame@gmail.com>
2020-08-02 16:21:51 -0400

    refactor: changed entry implementation

--- build/entry-file.js
+++ build/entry-file.js
@@ -4,8 +4,8 @@ const pascal = string => upperFirst(camelCase(string));
 
 class EntryFile {
 	apply(compiler) {
-		compiler.hooks.done.tapAsync('webpack-distsize-plugin', (stats, cb) => {
-			const iconExports = Array.from(stats.compilation.entrypoints.keys())
+		compiler.hooks.emit.tapAsync('webpack-distsize-plugin', (compilation, cb) => {
+			const iconExports = Array.from(compilation.entrypoints.keys())
 				.filter(p => p.startsWith('icons/'))
 				.map(p => {
 					const basename = path.basename(p);
@@ -15,8 +15,16 @@ class EntryFile {
 
 			iconExports.push('export { default } from \'./lib/icon-layer\'');
 
-			const {outputPath, outputFileSystem: fs} = compiler;
-			fs.writeFile(path.join(outputPath, 'index.js'), iconExports.join('\n'), cb);
+			const source = iconExports.join('\n');
+			compilation.assets['index.js'] = {
+				source() {
+					return source;
+				},
+				size() {
+					return source.length;
+				},
+			};
+			cb();
 		});
 	}
 }
